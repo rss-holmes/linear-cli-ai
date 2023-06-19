b8abb262db99f2e61da3ed65c2952c316de6647b
scar3crow <rss.holmes@gmail.com>
2023-06-19 17:49:13 +0530

    First commit

---
+++ linear.py
@@ -0,0 +1,268 @@
+import json
+
+import requests
+
+LINEAR_GRAPHQL_ENDPOINT = "https://api.linear.app/graphql"
+LINEAR_HEADER_AUTH = "lin_api_Td9dCCWAhYTvNkvQVhGINXkJvFUf5svccWE881oK"
+
+
+def make_linear_call(query: str) -> dict or None:
+    """
+    Function to make a call to linear
+    """
+
+    response = requests.post(
+        LINEAR_GRAPHQL_ENDPOINT,
+        json={"query": query},
+        headers={"Authorization": LINEAR_HEADER_AUTH},
+    )
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        return response_json
+    else:
+        print(response)
+        return None
+
+
+def get_team_list() -> list or None:
+    """
+    Get a list of all teams on linear
+    """
+
+    team_list_query = """
+        query Teams {
+        teams {
+            nodes {
+            id
+            name
+            }
+        }
+        }
+        """
+    response = make_linear_call(team_list_query)
+    if response:
+        values_list = response["data"]["teams"]["nodes"]
+        teams_list = [{**value, "value": i} for i, value in enumerate(values_list)]
+        return teams_list
+
+    return None
+
+
+def get_project_list() -> list or None:
+    """
+    Get a list of all projects on linear
+    """
+
+    project_list_query = """
+    query Projects {
+    projects(first: 10) {
+        nodes {
+        id
+        name
+        state
+        teams{
+                nodes {
+                id
+                name
+                }
+            }
+        }
+    }
+    }
+    """
+
+    response = make_linear_call(project_list_query)
+    if response:
+        values_list = response["data"]["projects"]["nodes"]
+        project_list = [{**value, "value": i} for i, value in enumerate(values_list)]
+        return project_list
+
+    return None
+
+
+def get_label_list() -> list or None:
+    """
+    Get a list of all labels on linear
+    """
+
+    label_list_query = """
+    query issueLabels {
+    issueLabels(first: 20) {
+        nodes {
+        id
+        name
+        color
+        }
+    }
+    }
+    """
+
+    response = make_linear_call(label_list_query)
+    if response:
+        values_list = response["data"]["issueLabels"]["nodes"]
+        label_list = [{**value, "value": i} for i, value in enumerate(values_list)]
+        return label_list
+
+    return None
+
+
+def get_user_list() -> list or None:
+    """
+    Get a list of all users on linear
+    """
+
+    user_list_query = """
+    query {
+        users {
+            nodes {
+            name
+            id
+            }
+        }
+    }
+    """
+
+    response = make_linear_call(user_list_query)
+    if response:
+        values_list = response["data"]["users"]["nodes"]
+        users_list = [{**value, "value": i} for i, value in enumerate(values_list)]
+        return users_list
+
+    return None
+
+
+def get_project_issue_list(self, project_id: str) -> list or None:
+    """
+    Get a list of all issues for a project on linear
+    """
+
+    issue_list_query = f"""
+    query Issues {{
+    issues(projectId: "{project_id}") {{
+        nodes {{
+        id
+        title
+        description
+        state
+        }}
+    }}
+    }}
+    """
+    response = requests.post(
+        self.url, json={"query": issue_list_query}, headers=self.headers
+    )
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        issues_list = response_json["data"]["issues"]["nodes"]
+        return issues_list
+    else:
+        return None
+
+
+def get_user_issue_list(self, user_id: str) -> list or None:
+    """
+    Get a list of all issues for a user on linear
+    """
+
+    issue_list_query = f"""
+    query Issues {{
+    issues(assigneeId: "{user_id}") {{
+        nodes {{
+        id
+        title
+        description
+        state
+        }}
+    }}
+    }}
+    """
+    response = requests.post(
+        self.url, json={"query": issue_list_query}, headers=self.headers
+    )
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        issues_list = response_json["data"]["issues"]["nodes"]
+        return issues_list
+    else:
+        return None
+
+
+def get_issue_details(self, issue_id: str) -> dict or None:
+    """
+    Get the details of an issue on linear
+    """
+
+    issue_details_query = f"""
+    query Issue {{
+    issue(id: "{issue_id}") {{
+        id
+        title
+        description
+        state
+        assignee {{
+            id
+            name
+        }}
+        project {{
+            id
+            name
+        }}
+        team {{
+            id
+            name
+        }}
+    }}
+    }}
+    """
+    response = requests.post(
+        self.url, json={"query": issue_details_query}, headers=self.headers
+    )
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        issue_details = response_json["data"]["issue"]
+        return issue_details
+    else:
+        return None
+
+
+def create_issue(
+    title: str,
+    description: str,
+    team_id: str,
+    project_id: str,
+    assignee_id: str,
+    label_ids,
+) -> None:
+    """
+    Function to create an issue on linear
+    """
+
+    print(title, description, team_id, project_id, assignee_id, label_ids)
+
+    query = f"""
+    mutation IssueCreate {{
+    issueCreate(
+        input: {{
+        title: "{title}"
+        description: "{description}"
+        teamId: "{team_id}"
+        projectId: "{project_id}"
+        assigneeId: "{assignee_id}"
+        labelIds: {json.dumps(label_ids)}
+        }}
+    ) {{
+        success
+        issue {{
+        id
+        title
+        }}
+    }}
+    }}
+    """
+    response = make_linear_call(query)
+    # print("issue create")
+    # print(response)
+    if response:
+        return True
+    else:
+        return False
---
+++ main.py
@@ -0,0 +1,197 @@
+import subprocess
+
+import typer
+from PyInquirer import Separator, print_json, prompt, style_from_dict
+from rich import print as rprint
+
+from linear import create_issue, get_project_list, get_team_list, get_user_list, get_label_list
+
+app = typer.Typer()
+
+# custom_style_2 = style_from_dict({
+#     "separator": '#6C6C6C',
+#     "questionmark": '#FF9D00 bold',
+#     "selected": '#5F819D',
+#     "pointer": '#FF9D00 bold',
+#     "instruction": '',  # default
+#     "answer": '#5F819D bold',
+#     "question": '',
+# })
+
+
+@app.command("hello")
+def sample_func(name: str):
+    rprint("[red bold]Hello[/red bold] [yellow]World[yello]")
+    module_list_question = [
+        {
+            "type": "list",
+            "name": "username",
+            "message": "Select any one username: ",
+            "choices": [
+                {
+                    "name": "Eddie",
+                },
+                {
+                    "name": "Hughie",
+                },
+                {
+                    "name": "Matthew ",
+                },
+                {
+                    "name": "Harvey ",
+                },
+            ],
+        },
+        {
+            "type": "list",
+            "name": "team",
+            "message": "Select any one team: ",
+            "choices": [
+                {
+                    "name": "Engineering",
+                },
+                {
+                    "name": "QA",
+                },
+                {
+                    "name": "Product ",
+                },
+            ],
+        },
+        {
+            "type": "input",
+            "name": "commit_name",
+            "message": "Give a commit name: ",
+        },
+        {
+            "type": "checkbox",
+            "name": "languages",
+            "message": "Select the languages: ",
+            "choices": [
+                {
+                    "name": "Python",
+                },
+                {
+                    "name": "Javascript",
+                },
+                {
+                    "name": "Go ",
+                },
+            ],
+        },
+        {
+            "type": "password",
+            "name": "password",
+            "message": "Give the password for the operation : ",
+        },
+        {
+            "type": "confirm",
+            "name": "confirmation",
+            "message": "Confirm all info is fine : ",
+        },
+    ]
+
+    username = prompt(module_list_question)
+
+    rprint("[yellow]=============================================[yellow]")
+    rprint("[green bold]Enter folder name :[green bold]")
+    folder_name = input()
+
+    print(f"The username is {username['username']}")
+    print(f"The team is {username['team']}")
+    print(f"The folder name is {folder_name}")
+
+
+@app.command("create-issue")
+def sample_func():
+    rprint("[yellow]Provide the following details : [yellow]")
+
+    # title: str, description: str, team_id: str, project_id: str, assignee_id: str
+
+    team_list = get_team_list()
+    user_list = get_user_list()
+    project_list = get_project_list()
+    label_list = get_label_list()
+    issue_creation_input_list = [
+        {
+            "type": "input",
+            "name": "title",
+            "message": "Title of the issue : ",
+        },
+        {
+            "type": "input",
+            "name": "description",
+            "message": "Description for the issue : ",
+        },
+        {
+            "type": "list",
+            "name": "team",
+            "message": "Select a team for the issue : ",
+            "choices": team_list,
+        },
+        {
+            "type": "list",
+            "name": "assignee",
+            "message": "Select an assignee for the issue : ",
+            "choices": user_list,
+        },
+        {
+            "type": "list",
+            "name": "project",
+            "message": "Select a project for the issue : ",
+            "choices": project_list,
+        },
+        {
+            "type": "checkbox",
+            "name": "labels",
+            "message": "Select the labels for the issue : ",
+            "choices": label_list,
+        },
+    ]
+
+    issue_data = prompt(issue_creation_input_list)
+
+    title = issue_data["title"]
+    description = issue_data["description"]
+    team_id = issue_data["team"]
+    assignee_id = issue_data["assignee"]
+    project_id = issue_data["project"]
+    labels = issue_data["labels"]
+
+    rprint("[yellow]=============================================[yellow]")
+    rprint("[green bold]Issue Creation Details :[green bold]")
+    rprint(f"[green bold]Title :[green bold] {title}")
+    rprint(f"[green bold]Description :[green bold] {description}")
+    rprint(f"[green bold]Team :[green bold] {team_list[team_id]['name'] }")
+    rprint(f"[green bold]Assignee :[green bold] {user_list[assignee_id]['name'] }")
+    rprint(f"[green bold]Project :[green bold] {project_list[project_id]['name'] }")
+    rprint(f"[green bold]Labels :[green bold] {[label_list[label]['name'] for label in labels]}")
+
+    issue_confirmation_list = [
+        {
+            "type": "confirm",
+            "name": "confirmation",
+            "message": "Confirm that the data is correct : ",
+        },
+    ]
+
+    confirmation_data = prompt(issue_confirmation_list)
+
+    if confirmation_data["confirmation"]:
+        rprint("[green bold]Creating the issue ... [green bold]")
+        issue_created = create_issue(
+            title,
+            description,
+            team_list[team_id]["id"],
+            project_list[project_id]["id"],
+            user_list[assignee_id]["id"],
+            [label_list[label]['id'] for label in labels],
+        )
+        if issue_created:
+            rprint("[green bold]Issue created successfully [green bold]")
+        else:
+            rprint("[red bold]Error while creating the issue [red bold]")
+
+
+if __name__ == "__main__":
+    app()
